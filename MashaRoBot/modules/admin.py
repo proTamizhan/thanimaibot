6e68508cfaa5cd1787fb14aa36a349b87a327e90
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-08-31 14:03:17 +0530

    Update admin.py

--- MashaRoBot/modules/admin.py
+++ MashaRoBot/modules/admin.py
@@ -690,7 +690,7 @@ dispatcher.add_handler(SETCHAT_TITLE_HANDLER)
 dispatcher.add_handler(SETSTICKET_HANDLER)
 dispatcher.add_handler(SETDESC_HANDLER)
 
-__mod_name__ = "ADMIN"
+__mod_name__ = "ADMIN 👨‍💻"
 __command_list__ = [
     "adminlist",
     "admins",
