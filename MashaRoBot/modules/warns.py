08fcd4988305cd16fc633110af472cda03dfe3b5
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-08-31 15:22:13 +0530

    Update warns.py

--- MashaRoBot/modules/warns.py
+++ MashaRoBot/modules/warns.py
@@ -522,7 +522,7 @@ be a sentence, encompass it with quotes, as such: `/addwarn "very angry" This is
  ❍ /strongwarn <on/yes/off/no>*:* If set to on, exceeding the warn limit will result in a ban. Else, will just punch.
 """
 
-__mod_name__ = "WA𝚁NS"
+__mod_name__ = "ᴡᴀʀɴS"
 
 WARN_HANDLER = CommandHandler(["warn", "dwarn"], warn_user, filters=Filters.group)
 RESET_WARN_HANDLER = CommandHandler(
