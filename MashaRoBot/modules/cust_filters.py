6ac8f8d2bb92464d0470894845bdfd05b4773514
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-09-15 09:49:56 +0530

    Update cust_filters.py

--- MashaRoBot/modules/cust_filters.py
+++ MashaRoBot/modules/cust_filters.py
@@ -639,7 +639,7 @@ Check ❍ /markdownhelp to know more!
 
 """
 
-__mod_name__ = "FILTERS"
+__mod_name__ = "ғɪʟᴛᴇʀ🤡"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
