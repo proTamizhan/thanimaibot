65e4bc25e4f4f18178af214c5c78c9afc0d1fb67
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-09-15 20:22:44 +0530

    BUTTON ERROR FIXED😊

--- MashaRoBot/modules/games.py
+++ MashaRoBot/modules/games.py
@@ -64,7 +64,7 @@ async def _(event):
 
 
 
-help = """
+__help__ = """
  *Play Game With Emojis:*
   - /dice or /dice 1 to 6 any value
   - /ball or /ball 1 to 5 any value
@@ -72,5 +72,4 @@ help = """
  Usage: hahaha just a magic.
  warning: you would be in trouble if you input any other value than mentioned.
 """
-
-mod_name = "GAMES"
+__mod_name__ = "GAMES"
