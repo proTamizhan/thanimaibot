8624f10bc0db806f6f252200d7043013d2a228e5
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-09-15 08:49:02 +0530

    Update dev.py

--- MashaRoBot/modules/dev.py
+++ MashaRoBot/modules/dev.py
@@ -90,5 +90,5 @@ dispatcher.add_handler(LEAVE_HANDLER)
 dispatcher.add_handler(GITPULL_HANDLER)
 dispatcher.add_handler(RESTART_HANDLER)
 
-__mod_name__ = "ᴅᴇᴠ🦁"
+__mod_name__ = "ᴅᴇᴠ👑"
 __handlers__ = [LEAVE_HANDLER, GITPULL_HANDLER, RESTART_HANDLER, ALLOWGROUPS_HANDLER]
