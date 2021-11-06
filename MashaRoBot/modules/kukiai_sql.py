ebf5c5fa2294d0cb4f964e92f89f2c9c70b925e5
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-11-06 17:49:28 +0530

    Update kukiai_sql.py

--- MashaRoBot/modules/kukiai_sql.py
+++ MashaRoBot/modules/kukiai_sql.py
@@ -1,7 +1,7 @@
 import threading
 
 from sqlalchemy import Column, String
-from KazukoBot.modules.sql import BASE, SESSION
+from MashaRoBot.modules.sql import BASE, SESSION
 
 class KukiChats(BASE):
     __tablename__ = "kuki_chats"
