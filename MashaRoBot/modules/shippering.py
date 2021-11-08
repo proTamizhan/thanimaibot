3ed571991e72fcc9be1da7faa97a87efdb06eb37
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-11-08 09:08:06 +0530

    Update shippering.py

--- MashaRoBot/modules/shippering.py
+++ MashaRoBot/modules/shippering.py
@@ -22,7 +22,7 @@ def dt():
 
 def dt_tom():
 
-    a = str(int(dt()[0].split('/')[0]) + 2)+"/" 
+    a = str(int(dt()[0].split('/')[0]) + 1)+"/"+1
 
         dt()[0].split('/')[1]+"/" + dt()[0].split('/')[2]
 
