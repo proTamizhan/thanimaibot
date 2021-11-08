364ad4cc3af8f7691444c6905c8f0023bf138669
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-11-08 19:12:30 +0530

    Update lydia_sql.py

--- MashaRoBot/modules/sql/lydia_sql.py
+++ MashaRoBot/modules/sql/lydia_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String
 
-from cinderella.modules.sql import BASE, SESSION
+from MashaRoBot.modules.sql import BASE, SESSION
 
 
 class ChatbotChats(BASE):
