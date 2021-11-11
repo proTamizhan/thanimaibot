9d7159dc8f7c295c93d0cea3ff51bcaee02d2340
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-11-11 08:14:46 +0530

    Update logomaker.py

--- MashaRoBot/modules/logomaker.py
+++ MashaRoBot/modules/logomaker.py
@@ -130,7 +130,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./innexiaBot/resources/Maghrib.ttf", 1000)
+    font = ImageFont.truetype("./MashaRoBot/resources/Maghrib.ttf", 1000)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
