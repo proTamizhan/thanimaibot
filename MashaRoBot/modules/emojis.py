66ab645877c48293a7be8ae9dae27327383a4867
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-11-11 08:26:31 +0530

    Update emojis.py

--- MashaRoBot/modules/emojis.py
+++ MashaRoBot/modules/emojis.py
@@ -374,6 +374,6 @@ dispatcher.add_handler(CLOCKANIMATION_HANDLER)
 dispatcher.add_handler(BLOCKANIMATION_HANDLER)
 
 
-__mod_name__ = "EMOJIS"
+__mod_name__ = "ᴇᴍᴏᴊɪ😻"
 __command_list__ = ["love", "hack", "bombs", "moonanimation", "clockanimation", "earthanimation", "blockanimation", "kill"]
 __handlers__ = [LOVE_HANDLER, HACK_HANDLER, BOMBS_HANDLER, MOONANIMATION_HANDLER, CLOCKANIMATION_HANDLER, EARTHANIMATION_HANDLER, BLOCKANIMATION_HANDLER, KILL_HANDLER]
