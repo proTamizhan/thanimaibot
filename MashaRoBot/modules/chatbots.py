61e4c18db270b608573abf53bcf475111a957d13
おい aaman <heyaaman2004@gmail.com>
2021-11-11 14:42:51 +0530

    Create chatbots.py

---
+++ MashaRoBot/modules/chatbots.py
@@ -0,0 +1,7 @@
+__help__= f"""
+*Athena AI Chatbot* 
+/addchat : Enables and Disables kazuko AI Chat mode (EXCLUSIVE)
+*Powered by* : [Kuki Chatbot](https://github.com/MoeZilla/KukiChatbot)
+"""
+
+__mod_name__ = "ᴄʜᴀᴛʙᴏᴛ🤓"
