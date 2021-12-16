70489a7998622a9d3b2b2b6c8d0ebb3b087c8006
proTamizhan <96135619+proTamizhan@users.noreply.github.com>
2021-12-16 22:34:03 +0530

    Update disasters.py

--- MashaRoBot/modules/disasters.py
+++ MashaRoBot/modules/disasters.py
@@ -645,7 +645,7 @@ Group admins/group owners do not need these commands.
  ❍ /logs Get heroku dyno logs.
 
 `⚠️ Read from top`
-Visit *@Athena_support* for more information.
+Visit *@thanimaisupport* for more information.
 """
 
 SUDO_HANDLER = CommandHandler(("addsudo", "adddragon"), addsudo)
