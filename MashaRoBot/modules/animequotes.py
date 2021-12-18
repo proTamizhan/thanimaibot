46a7313b4ea6827391589d84fee2c383496f72f9
Kittuthemeow <89501967+VipBoys777@users.noreply.github.com>
2021-12-18 11:22:18 +0530

    Update animequotes.py

--- MashaRoBot/modules/animequotes.py
+++ MashaRoBot/modules/animequotes.py
@@ -6,14 +6,14 @@ from telegram import ParseMode, Update, ChatPermissions
 from telegram.ext import CallbackContext, run_async
 from telegram.error import BadRequest
 
-import MashaRoBot.modules.animequotes_strings as animequotes_strings
-from MashaRoBot import dispatcher
-from MashaRoBot.modules.disable import DisableAbleCommandHandler
-from MashaRoBot.modules.helper_funcs.chat_status import (is_user_admin)
-from MashaRoBot.modules.helper_funcs.extraction import extract_user
+import GroupMenter.modules.animequotes_strings as animequotes_strings
+from GroupMenter import dispatcher
+from GroupMenter.modules.disable import DisableAbleCommandHandler
+from GroupMenter.modules.helper_funcs.chat_status import (is_user_admin)
+from GroupMenter.modules.helper_funcs.extraction import extract_user
 
 @run_async
-def animequotes(update: Update, context: CallbackContext):
+def fun(update: Update, context: CallbackContext):
     message = update.effective_message
     name = message.reply_to_message.from_user.first_name if message.reply_to_message else message.from_user.first_name
     reply_photo = message.reply_to_message.reply_photo if message.reply_to_message else message.reply_photo
@@ -21,16 +21,16 @@ def animequotes(update: Update, context: CallbackContext):
         random.choice(animequotes_strings.QUOTES_IMG))
 
 __help__ = """
- • `/animequotes`*:* gives random anime quotes
+ • `/fun`*:* gives random tamil memes
  
 """
-ANIMEQUOTES_HANDLER = DisableAbleCommandHandler("animequotes", animequotes)
+ANIMEQUOTES_HANDLER = DisableAbleCommandHandler("fun", fun)
 
 dispatcher.add_handler(ANIMEQUOTES_HANDLER)
 
-__mod_name__ = "ᴀɴɪᴍᴇ ǫᴜᴏᴛᴇ🦋"
+__mod_name__ = "ᴛᴀᴍɪʟ ᴍᴇᴍᴇs"
 __command_list__ = [
-    "animequotes"
+    "fun"
 ]
 __handlers__ = [
     ANIMEQUOTES_HANDLER
