8f4b5521f277f2665e5b1f4ac82c6a7a7eb08117
Kittuthemeow <89501967+VipBoys777@users.noreply.github.com>
2021-12-18 11:22:49 +0530

    Update animequotes_strings.py

--- MashaRoBot/modules/animequotes_strings.py
+++ MashaRoBot/modules/animequotes_strings.py
@@ -1,68 +1,114 @@
 QUOTES_IMG = (
-      "https://i.imgur.com/Iub4RYj.jpg", 
-      "https://i.imgur.com/uvNMdIl.jpg", 
-      "https://i.imgur.com/YOBOntg.jpg", 
-      "https://i.imgur.com/fFpO2ZQ.jpg", 
-      "https://i.imgur.com/f0xZceK.jpg", 
-      "https://i.imgur.com/RlVcCip.jpg", 
-      "https://i.imgur.com/CjpqLRF.jpg", 
-      "https://i.imgur.com/8BHZDk6.jpg", 
-      "https://i.imgur.com/8bHeMgy.jpg", 
-      "https://i.imgur.com/5K3lMvr.jpg", 
-      "https://i.imgur.com/NTzw4RN.jpg", 
-      "https://i.imgur.com/wJxryAn.jpg", 
-      "https://i.imgur.com/9L0DWzC.jpg", 
-      "https://i.imgur.com/sBe8TTs.jpg", 
-      "https://i.imgur.com/1Au8gdf.jpg", 
-      "https://i.imgur.com/28hFQeU.jpg", 
-      "https://i.imgur.com/Qvc03JY.jpg", 
-      "https://i.imgur.com/gSX6Xlf.jpg", 
-      "https://i.imgur.com/iP26Hwa.jpg", 
-      "https://i.imgur.com/uSsJoX8.jpg", 
-      "https://i.imgur.com/OvX3oHB.jpg", 
-      "https://i.imgur.com/JMWuksm.jpg", 
-      "https://i.imgur.com/lhM3fib.jpg", 
-      "https://i.imgur.com/64IYKkw.jpg", 
-      "https://i.imgur.com/nMbyA3J.jpg", 
-      "https://i.imgur.com/7KFQhY3.jpg", 
-      "https://i.imgur.com/mlKb7zt.jpg", 
-      "https://i.imgur.com/JCQGJVw.jpg", 
-      "https://i.imgur.com/hSFYDEz.jpg", 
-      "https://i.imgur.com/PQRjAgl.jpg", 
-      "https://i.imgur.com/ot9624U.jpg", 
-      "https://i.imgur.com/iXmqN9y.jpg", 
-      "https://i.imgur.com/RhNBeGr.jpg", 
-      "https://i.imgur.com/tcMVNa8.jpg", 
-      "https://i.imgur.com/LrVg810.jpg", 
-      "https://i.imgur.com/TcWfQlz.jpg", 
-      "https://i.imgur.com/muAUdvJ.jpg", 
-      "https://i.imgur.com/AtC7ZRV.jpg", 
-      "https://i.imgur.com/sCObQCQ.jpg", 
-      "https://i.imgur.com/AJFDI1r.jpg", 
-      "https://i.imgur.com/TCgmRrH.jpg", 
-      "https://i.imgur.com/LMdmhJU.jpg", 
-      "https://i.imgur.com/eyyax0N.jpg", 
-      "https://i.imgur.com/YtYxV66.jpg", 
-      "https://i.imgur.com/292w4ye.jpg", 
-      "https://i.imgur.com/6Fm1vdw.jpg", 
-      "https://i.imgur.com/2vnBOZd.jpg", 
-      "https://i.imgur.com/j5hI9Eb.jpg", 
-      "https://i.imgur.com/cAv7pJB.jpg", 
-      "https://i.imgur.com/jvI7Vil.jpg", 
-      "https://i.imgur.com/fANpjsg.jpg", 
-      "https://i.imgur.com/5o1SJyo.jpg", 
-      "https://i.imgur.com/dSVxmh8.jpg", 
-      "https://i.imgur.com/02dXlAD.jpg", 
-      "https://i.imgur.com/htvIoGY.jpg", 
-      "https://i.imgur.com/hy6BXOj.jpg", 
-      "https://i.imgur.com/OuwzNYu.jpg", 
-      "https://i.imgur.com/L8vwvc2.jpg", 
-      "https://i.imgur.com/3VMVF9y.jpg", 
-      "https://i.imgur.com/yzjq2n2.jpg", 
-      "https://i.imgur.com/0qK7TAN.jpg", 
-      "https://i.imgur.com/zvcxSOX.jpg", 
-      "https://i.imgur.com/FO7bApW.jpg", 
-      "https://i.imgur.com/KK06gwg.jpg", 
-      "https://i.imgur.com/6lG4tsO.jpg"
-      
-      ) 
+"https://telegra.ph/file/5f414b8f0a4b4f316f894.jpg",
+"https://telegra.ph/file/73996272d459c6ea16bf7.jpg",
+"https://telegra.ph/file/941cde9db0b26d69703f4.jpg",
+
+"https://telegra.ph/file/7ed6f4224d4acd08173c.jpg",
+
+"https://telegra.ph/file/8623d5607d975e148a0cf.jpg",
+
+"https://telegra.ph/file/a10b527b2a0f5e382b2fc.jpg",
+
+"https://telegra.ph/file/90f5114486ff5f5fd8ea2.jpg",
+
+"https://telegra.ph/file/762f3a13bc5e1fbdbd2b3.jpg",
+
+"https://telegra.ph/file/87091f2dd1d010ccbadd2.jpg",
+
+"https://telegra.ph/file/95d2d5698519544f686ec.jpg",
+
+"https://telegra.ph/file/936d8251e3eda4c1662c4.jpg",
+
+"https://telegra.ph/file/b73c93f2a1ae67061f9a6.jpg",
+
+"https://telegra.ph/file/557def50ee49ec240b2c4.jpg",
+
+"https://telegra.ph/file/2f410d8ecfe3dc925aa2c.jpg",
+
+"https://telegra.ph/file/238ebb7d45a834405af52.jpg",
+
+"https://telegra.ph/file/0de71fbe0519bb0c19b99.jpg",
+
+"https://telegra.ph/file/6b85aa5aa7850e179f688.jpg",
+
+"https://telegra.ph/file/18382ea967fbe7f581017.jpg",
+
+"https://telegra.ph/file/107ce84d0479aafbc589f.jpg",
+
+"https://telegra.ph/file/809079907a93fffe8b1e2.jpg",
+
+"https://telegra.ph/file/0f5cf6e56c624c579eebc.jpg",
+
+"https://telegra.ph/file/2956eb1cdd25c767076c4.jpg",
+
+"https://telegra.ph/file/be1dcb843810d3b8b1bec.jpg",
+
+"https://telegra.ph/file/b6f1a68d296e5280da321.jpg",
+
+"https://telegra.ph/file/fbde8b6e6cbd5a9ac050f.jpg",
+
+"https://telegra.ph/file/af046ce053eed7c01d117.jpg",
+
+"https://telegra.ph/file/1cdb4663acd35b2fa3f65.jpg",
+
+"https://telegra.ph/file/95f6f492fefb413124eb3.jpg",
+
+"https://telegra.ph/file/35bd93d90181c3fa5a875.jpg",
+
+"https://telegra.ph/file/e3a78917d3274115e4f8c.jpg",
+
+"https://telegra.ph/file/651662a759e635077d573.jpg",
+
+"https://telegra.ph/file/2f60ff5e98bfe1b8d13fe.jpg",
+
+"https://telegra.ph/file/741044f3fbd1d1e3acf8d.jpg",
+
+"https://telegra.ph/file/cfd17959b1b5bb2421be0.jpg",
+
+"https://telegra.ph/file/802a84f102f7d237e298c.jpg",
+
+"https://telegra.ph/file/0c2c8d9da6ea6de6d9503.jpg",
+
+"https://telegra.ph/file/4349482d666a19378209d.jpg",
+
+"https://telegra.ph/file/266b903a859480d301e53.jpg",
+
+"https://telegra.ph/file/c453e3942b1f6616f40cf.jpg",
+
+"https://telegra.ph/file/530f06e37ef4ef9d9ec68.jpg",
+
+"https://telegra.ph/file/581b84f9ebf7b1f0c73b0.jpg",
+
+"https://telegra.ph/file/310183b8da7b9d436619d.jpg",
+
+"https://telegra.ph/file/c5788b7bd27771cc7cb49.jpg",
+
+"https://telegra.ph/file/32b7bcc8fcfad1994e3d4.jpg",
+
+"https://telegra.ph/file/dcf53740a7510f44d9a5b.jpg",
+
+"https://telegra.ph/file/d5dfef127f805cd4341f3.jpg",
+
+"https://telegra.ph/file/77bef07d4be70dd257fc1.jpg",
+
+"https://telegra.ph/file/c36fe38a23512b2f6df6a.jpg",
+
+"https://telegra.ph/file/028751fd9b8750e9891f9.jpg",
+
+"https://telegra.ph/file/cc50ecbbbdc28ac9b2228.jpg",
+
+"https://telegra.ph/file/142a61347f2f4fbd258c9.jpg",
+
+"https://telegra.ph/file/c7040297d2487f1364230.jpg",
+
+"https://telegra.ph/file/f77a80db0e08eee179c43.jpg",
+
+"https://telegra.ph/file/97a15113108f3ecd93496.jpg",
+
+"https://telegra.ph/file/290a1f7a9147bf01e3b67.jpg",
+
+"https://telegra.ph/file/0a493a694626a4deee936.jpg",
+
+"https://telegra.ph/file/10ea2d1f709632e8b5316.jpg",
+
+ )
