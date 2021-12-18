7a3cb08a3a3f0fbfbb8f6a90be6bf6bcbece2267
proTamizhan <96135619+proTamizhan@users.noreply.github.com>
2021-12-18 19:35:12 +0530

    Update userinfo.py

--- MashaRoBot/modules/userinfo.py
+++ MashaRoBot/modules/userinfo.py
@@ -306,7 +306,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' [<a href="https://t.me/Mastro_updates/7">?</a>]'.format(
+        text += ' [<a href="https://t.me/ThETeLeGrAmPrO">?</a>]'.format(
             bot.username
         )
 
