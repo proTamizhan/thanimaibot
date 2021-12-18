10fc27aa69b0909c71c10f267a9b94ae2126241e
proTamizhan <96135619+proTamizhan@users.noreply.github.com>
2021-12-18 22:10:48 +0530

    Update __sg.py

--- MashaRoBot/modules/__sg.py
+++ MashaRoBot/modules/__sg.py
@@ -181,7 +181,6 @@ dispatcher.add_handler(CommandHandler("search_id",search_id))
 dispatcher.add_handler(CommandHandler("check_name",check_name))
 dispatcher.add_handler(CommandHandler("check_username",check_username))
 dispatcher.add_handler(CommandHandler("check_brain",check_brain))
-dispatcher.add_handler(MessageHandler(Filters.chat_type.private,start))
 dispatcher.add_error_handler(error)
 
 
