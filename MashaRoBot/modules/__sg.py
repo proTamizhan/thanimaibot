48fc32490d4bfba8ef69fd0e03bb7b8e250dac17
proTamizhan <96135619+proTamizhan@users.noreply.github.com>
2021-12-18 22:01:20 +0530

    Update __sg.py

--- MashaRoBot/modules/__sg.py
+++ MashaRoBot/modules/__sg.py
@@ -176,8 +176,6 @@ def error(update, context):
   
 
 
-
-dispatcher.add_handler(MessageHandler(Filters.chat.private Filters.forwarded,Forwarded))
 dispatcher.add_handler(CommandHandler("start",start))
 dispatcher.add_handler(CommandHandler("search_id",search_id))
 dispatcher.add_handler(CommandHandler("check_name",check_name))
