1bcd6a645842610852b9e7a80585795e2f032f80
proTamizhan <96135619+proTamizhan@users.noreply.github.com>
2021-12-18 20:19:30 +0530

    Update handlers.py

--- MashaRoBot/modules/helper_funcs/handlers.py
+++ MashaRoBot/modules/helper_funcs/handlers.py
@@ -18,7 +18,15 @@ else:
     CMD_STARTERS = ("/",)
 
 
-
+class AntiSpam:
+    def __init__(self):
+        self.whitelist = (
+            (DEV_USERS or [])
+            + (DRAGONS or [])
+            + (WOLVES or [])
+            + (DEMONS or [])
+            + (TIGERS or [])
+        )
         # Values are HIGHLY experimental, its recommended you pay attention to our commits as we will be adjusting the values over time with what suits best.
         Duration.CUSTOM = 15  # Custom duration, 15 seconds
         self.sec_limit = RequestRate(6, Duration.CUSTOM)  # 6 / Per 15 Seconds
