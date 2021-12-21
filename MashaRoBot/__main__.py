b3fa4378fa5d50369b7dc4c26e47605cdf5c3504
proTamizhan <96135619+proTamizhan@users.noreply.github.com>
2021-12-21 20:46:53 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -726,7 +726,11 @@ def main():
         telethn.run_until_disconnected()
 
     updater.idle()
-
+try:
+    ubot.start()
+except BaseException:
+    print("Userbot Error! Have you added a STRING_SESSION in deploying??")
+    sys.exit(1)
 
 if __name__ == "__main__":
     LOGGER.info("Successfully loaded modules: " + str(ALL_MODULES))
