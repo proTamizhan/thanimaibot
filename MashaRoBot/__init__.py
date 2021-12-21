d2c2fbb6996aa7e7577f7080f6cda2f7d798db23
proTamizhan <96135619+proTamizhan@users.noreply.github.com>
2021-12-21 22:38:18 +0530

    Update __init__.py

--- MashaRoBot/__init__.py
+++ MashaRoBot/__init__.py
@@ -121,7 +121,7 @@ if ENV:
             "Your blacklisted chats list does not contain valid integers.")
 
 else:
-    from RemiRobot.config import Development as Config
+    from MashaRoBot.config import Development as Config
     TOKEN = Config.TOKEN
 
     try:
