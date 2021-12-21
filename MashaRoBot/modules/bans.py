77ec1434b9080c9c852e329dbfe82f70d59836bc
proTamizhan <96135619+proTamizhan@users.noreply.github.com>
2021-12-21 07:45:39 +0530

    Update bans.py

--- MashaRoBot/modules/bans.py
+++ MashaRoBot/modules/bans.py
@@ -545,7 +545,7 @@ dispatcher.add_handler(UNBAN_HANDLER)
 dispatcher.add_handler(ROAR_HANDLER)
 dispatcher.add_handler(PUNCHME_HANDLER)
 
-__mod_name__ = "📛Ban/Mute"
+__mod_name__ = "ʙᴀɴ/ᴍᴜᴛᴇs💖"
 __handlers__ = [
     BAN_HANDLER,
 
