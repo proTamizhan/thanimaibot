334e55685b1d4f873f4c7a5800c5de0f35c2ae82
proTamizhan <96135619+proTamizhan@users.noreply.github.com>
2021-12-28 16:57:04 +0530

    Update config.py

--- MashaRoBot/config.py
+++ MashaRoBot/config.py
@@ -1,181 +1,62 @@
-# This file is part of Daisy (Telegram Bot)
-
-# This program is free software: you can redistribute it and/or modify
-
-# it under the terms of the GNU Affero General Public License as
-
-# published by the Free Software Foundation, either version 3 of the
-
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+import json
 import os
 
-import sys
 
-import yaml
-
-from envparse import env
-
-from MashaRoBot.utils.logger import log
-
-DEFAULTS = {
-
-    "LOAD_MODULES": True,
-
-    "DEBUG_MODE": True,
-
-    "REDIS_HOST": "localhost",
-
-    "REDIS_PORT": 6379,
-
-    "REDIS_DB_FSM": 1,
-
-    "MONGODB_URI": "localhost",
-
-    "MONGO_DB": "DaisyX",
-
-    "API_PORT": 8080,
-
-    "JOIN_CONFIRM_DURATION": "30m",
-
-}
-
-CONFIG_PATH = "data/bot_conf.yaml"
-
-if os.name == "nt":
-
-    log.debug("Detected Windows, changing config path...")
-
-    CONFIG_PATH = os.getcwd() + "\\data\\bot_conf.yaml"
-
-if os.path.isfile(CONFIG_PATH):
-
-    log.info(CONFIG_PATH)
-
-    for item in (
-
-        data := yaml.load(open("data/bot_conf.yaml", "r"), Loader=yaml.CLoader)
-
-    ):
-
-        DEFAULTS[item.upper()] = data[item]
-
-else:
-
-    log.info("Using env vars")
-
-def get_str_key(name, required=False):
-
-    if name in DEFAULTS:
-
-        default = DEFAULTS[name]
-
-    else:
-
-        default = None
-
-    if not (data := env.str(name, default=default)) and not required:
-
-        log.warn("No str key: " + name)
-
-        return None
-
-    elif not data:
-
-        log.critical("No str key: " + name)
-
-        sys.exit(2)
-
-    else:
-
-        return data
-
-def get_int_key(name, required=False):
-
-    if name in DEFAULTS:
-
-        default = DEFAULTS[name]
-
-    else:
-
-        default = None
-
-    if not (data := env.int(name, default=default)) and not required:
-
-        log.warn("No int key: " + name)
-
-        return None
-
-    elif not data:
-
-        log.critical("No int key: " + name)
-
-        sys.exit(2)
-
-    else:
-
-        return data
-
-def get_list_key(name, required=False):
-
-    if name in DEFAULTS:
-
-        default = DEFAULTS[name]
-
-    else:
-
-        default = None
-
-    if not (data := env.list(name, default=default)) and not required:
-
-        log.warn("No list key: " + name)
-
-        return []
-
-    elif not data:
-
-        log.critical("No list key: " + name)
-
-        sys.exit(2)
-
-    else:
-
-        return data
-
-def get_bool_key(name, required=False):
-
-    if name in DEFAULTS:
-
-        default = DEFAULTS[name]
-
-    else:
-
-        default = None
-
-    if not (data := env.bool(name, default=default)) and not required:
-
-        log.warn("No bool key: " + name)
-
-        return False
-
-    elif not data:
-
-        log.critical("No bool key: " + name)
-
-        sys.exit(2)
-
-    else:
-
-        return data
+def get_user_list(config, key):
+    with open('{}/MashaRoBot/{}'.format(os.getcwd(), config),
+              'r') as json_file:
+        return json.load(json_file)[key]
+
+class Config(object):
+    LOGGER = True
+
+    API_ID = "FILL API ID "  # integer value, dont use ""
+    API_HASH = "FIL API HASH"
+    TOKEN = "BOT_TOKEN"
+    OWNER_ID = 1732814103  
+    OWNER_USERNAME = "theTelegrampro"
+    SUPPORT_CHAT = 'Thanimaisupport'
+    JOIN_LOGGER = -1001739802989
+    EVENT_LOGS = -1001739802989
+
+    #RE
+    SQLALCHEMY_DATABASE_URI = 'ELEPHAT OR HEROKU DB URL'  
+    LOAD = []
+    NO_LOAD = ['rss', 'cleaner', 'connection', 'math']
+    WEBHOOK = None
+    INFOPIC = True
+    URL = None
+    SPAMWATCH_API = ""  
+    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
+    BOT_ID = "2052309535"
+    
+    DRAGONS = get_user_list('elevated_users.json', 'sudos')
+
+    DEV_USERS = get_user_list('elevated_users.json', 'devs')
+    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    DEMONS = get_user_list('elevated_users.json', 'supports')
+    #List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    TIGERS = get_user_list('elevated_users.json', 'tigers')
+    WOLVES = get_user_list('elevated_users.json', 'whitelists')
+    DONATION_LINK = None  # EG, paypal
+    CERT_PATH = None
+    PORT = 5000
+    DEL_CMDS = True  #Delete commands that users dont have access to, like delete /ban if a non admin uses it.
+    STRICT_GBAN = True
+    WORKERS = 8  # Number of subthreads to use. Set as number of threads your processor uses
+    BAN_STICKER = ''  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
+    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
+    CASH_API_KEY = 'awoo'  # Get your API key from https://www.alphavantage.co/support/#api-key
+    TIME_API_KEY = 'awoo'  # Get your API key from https://timezonedb.com/api
+    WALL_API = 'awoo'  #For wallpapers, get one from https://wall.alphacoders.com/api.php
+    AI_API_KEY = 'awoo'  #For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
+    BL_CHATS = []  # List of groups that you want blacklisted.
+    SPAMMERS = None
+
+
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True
