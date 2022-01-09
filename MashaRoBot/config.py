4f5ba772b6994becdff35ca5fff0defc84c43b81
proTamizhan <96135619+proTamizhan@users.noreply.github.com>
2022-01-09 14:46:23 +0530

    Update config.py

--- MashaRoBot/config.py
+++ MashaRoBot/config.py
@@ -11,7 +11,7 @@ class Config(object):
     LOGGER = True
   
     TEMP_DOWNLOAD_DIRECTORY = 'None'
-    API_ID = "FILL API ID "  # integer value, dont use ""
+    API_ID = FILL API ID
     API_HASH = "FIL API HASH"
     TOKEN = "BOT_TOKEN"
     OWNER_ID = 1732814103  
